5ef524e8b40241ace6d452943eaf32b11e6b4bc7
ng <naoufal.gaffa@gmail.com>
2021-01-20 21:48:11 +0100

    add the s3.tf to describe s3 bucket

---
+++ s3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "s3m-bucket-angular" {
+  bucket = "s3m-bucket-angular"
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+  policy = file("policies/s3-policy.json")
+  tags = {
+    Name        = "s3m-bucket-angular"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3m-bucket-angular-ab" {
+  bucket = aws_s3_bucket.s3m-bucket-angular.id
+  block_public_acls   = true
+  block_public_policy = true
+}
