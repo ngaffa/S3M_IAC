640140fc0847200dba45f4ba3053b644eff89f53
ng <naoufal.gaffa@gmail.com>
2021-02-08 00:43:30 +0100

    fixing lambda authorizer file

--- authorizer.tf
+++ authorizer.tf
@@ -2,7 +2,7 @@ resource "aws_api_gateway_authorizer" "s3m-ag-authorizer" {
   name                   = "s3m-ag-authorizer"
   rest_api_id            = aws_api_gateway_rest_api.s3m-ag-proxy.id
   authorizer_uri         = aws_lambda_function.s3m-lambda-authorizer.invoke_arn
-  authorizer_credentials = aws_iam_role.s3m-authorizer-invocation-role.arn
+  # authorizer_credentials = aws_iam_role.s3m-authorizer-invocation-role.arn
 
 }
 resource "aws_iam_role" "s3m-authorizer-invocation-role" {
@@ -12,10 +12,10 @@ resource "aws_iam_role" "s3m-authorizer-invocation-role" {
 }
 
 resource "aws_iam_role_policy" "s3m-authorizer-invocation-policy" {
-  name = "s3m-authorizer-invocation-polic"
+  name = "s3m-authorizer-invocation-policy"
   role = aws_iam_role.s3m-authorizer-invocation-role.id
 
-  policy = file("policies/invocation-policy.json")
+  policy = replace(file("policies/invocation-policy.json"),"__LAMBDA_ARN__",aws_lambda_function.s3m-lambda-authorizer.arn)
 }
 
 resource "aws_iam_role" "s3m-lambda-role" {
@@ -24,11 +24,15 @@ resource "aws_iam_role" "s3m-lambda-role" {
   assume_role_policy = file("policies/lambda-policy.json")
   }
 
+
 resource "aws_lambda_function" "s3m-lambda-authorizer" {
-  filename      = "s3m-lambda-authorizer.zip"
+  filename      = "lambda-code/s3m-lambda-authorizer.zip"
   function_name = "s3m-lambda-authorizer"
   role          = aws_iam_role.s3m-lambda-role.arn
   handler       = "exports.handler"
-
+  runtime = "nodejs12.x"
   source_code_hash = filebase64sha256("lambda-code/s3m-lambda-authorizer.zip")
+  #"${base64sha256(filebase64("lambda-code/s3m-lambda-authorizer.zip"))}"
+  #sha256(filebase64("lambda-code/s3m-lambda-authorizer.zip"))
+  # filebase64sha256("lambda-code/s3m-lambda-authorizer.zip")
 }
